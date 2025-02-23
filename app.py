708abf59abfa50c1e5824a753d192003cd47f9eb
zakharovarina <zakharovarina@gmail.com>
2025-02-23 15:25:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import streamlit as st
 
 st.header('Tossing a Coin')
 
 st.write('It is not a functional application yet. Under construction.')
-=======
-import streamlit as st
-
-st.header('Tossing a Coin')
-
-st.write('It is not a functional application yet. Under construction.')
->>>>>>> b3488b827b6d696a845f7a0269c34aa59acda034
